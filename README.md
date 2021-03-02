342d6b985faa01c5de69ac9ce59cce7e07acd388
Linda Naughton <lynn@wordsmyth.org>
2021-03-02 14:03:21 -0500

    Initial draft.

---
+++ README.md
@@ -0,0 +1,47 @@
+
+# RKStudio REST API Quickstart Server
+
+This app is a demonstration of how to access the [RKStudio REST API](https://developer.rkstudio.careevolution.com/api/) using [Node.js](https://nodejs.org/en/). It gives an example of obtaining an access token and making a simple query to the API. You can use this app for reference, or modify it to test out your own API requests in a development environment.
+
+## Prerequisites
+
+Before you begin, you will need three things:
+
+* Your RKStudio service account name, like “RKStudio.1234.test.”
+* Your RKStudio project ID, which is a GUID.
+* The private key you associated with your service account.
+
+For help finding this information, see the [RKStudio Developer Docs](https://developer.rkstudio.careevolution.com/api/quickstart.html).
+
+## Using the App
+
+This is a command-line app in Node.js. To run the app:
+
+1. Install NodeJS and the Node Package Manager (npm). One way to do this is using the Chocolatey software manager. Just install Chocolatey and execute this command in an administrator Powershell window: `choco install -y nodejs`.
+2. Clone this repository.
+3. Copy the file `dotenv.sample` and name the new copy `.env`.
+4. Edit the `.env` file and fill in your project ID, service account name, and private key. See **Prerequisites** above for more information. Be sure to include the begin/end tags “--BEGIN RSA PRIVATE KEY--“ and “--END RSA PRIVATE KEY--" in your private key.
+5. Open a Powershell window in the application directory.
+6. Install the necessary npm libraries: `npm install`.
+6. Run the script: `node quickstart.js`.
+
+If successful, the app will print out your token and number of participants in the console, like so:
+
+```
+Obtained access token:
+  YOUR TOKEN HERE
+  
+Total Participants: 5
+```
+
+The access token is only valid for a few minutes, but you can copy/paste it into a REST query tool of your choice to try out advanced queries.
+
+## Troubleshooting
+
+If you see an error when running the script, double-check the information in the Prerequisites, particularly the format of the private key. It should include the begin/end tags and line breaks, looking something like this:
+
+```
+RKS_PRIVATE_KEY="-----BEGIN RSA PRIVATE KEY-----\n230703de230703de230703de\nb62b0e24b62b0e24b62b0e24\n...\n-----END RSA PRIVATE KEY-----"
+```
+
+If you have trouble getting the app to work, feel free to [contact RKStudio Support](https://developer.rkstudio.careevolution.com/help/).
---
+++ quickstart.js
@@ -0,0 +1,111 @@
+require('dotenv').config();
+
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+const util = require('util');
+const querystring = require('querystring');
+
+// **NOTE!** In a real production app you would want these to be sourced from real environment variables. The .env file is just
+// a convenience for development.
+const rksProjectId = process.env.RKS_PROJECT_ID;
+const rksServiceAccount = process.env.RKS_SERVICE_ACCOUNT;
+const privateKey = process.env.RKS_PRIVATE_KEY;
+
+const baseApiUri = 'https://rkstudio.careevolution.com/inv';
+
+async function getFromApi(accessToken, resourceUrl, queryParams = {}) {
+  var data = null;
+  let api = axios.create({
+    baseURL: baseApiUri,
+    headers: {
+      "Authorization": `Bearer ${accessToken}`,
+      "Accept": "application/json", 
+      "Content-Type": "application/json; charset=utf-8"
+    }
+  });
+  
+  await api.get(resourceUrl, { params: queryParams })
+  .then(function (apiResponse) {
+    if (apiResponse.status != '200') {
+      logResponse(apiResponse);
+    }
+    else {
+      data = apiResponse.data;
+    }})
+  .catch(function (error) {
+    logResponse(error);
+  });
+  return data;
+}
+
+async function getAccessToken() {
+    const audienceString = `${baseApiUri}/identityserver/connect/token`;
+    
+    const assertion = {
+        "iss": rksServiceAccount,
+        "sub": rksServiceAccount,
+        "aud": audienceString,
+        "exp": Math.floor(new Date().getTime() / 1000) + 200,
+        "jti": "random-non-reusable-jwt-id-123"
+    };
+
+    var signedAssertion;
+    try {
+        signedAssertion = jwt.sign(assertion, privateKey, { algorithm: 'RS256' });
+    }
+    catch(err) {
+      console.log(`Error signing JWT. Check your private key. Error: ${err}`);
+      return null;
+    }
+    
+    const payload = {
+        scope: "api",
+        grant_type: "client_credentials",
+        client_assertion_type: "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+        client_assertion: signedAssertion
+    };
+
+    const tokenResponse = await makeAccessTokenRequest(payload); 
+    if (!tokenResponse || !tokenResponse.access_token) {
+        return null;
+    }
+    return tokenResponse.access_token;
+}
+
+
+async function makeAccessTokenRequest(payload) {
+  return axios.post(`${baseApiUri}/identityserver/connect/token`, querystring.stringify(payload))
+  .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+      return null;
+    });
+}
+
+function logResponse(response) {
+  console.log(util.inspect(response, { colors: true, depth: 3 }));
+}
+
+getAccessToken().then( token => {
+  if (token) {
+    console.log('Obtained access token:');
+    console.log(token);
+    
+    const participantResourceUrl = `/api/v1/administration/projects/` + rksProjectId + '/participants';
+    getFromApi(token, participantResourceUrl)
+      .then(data => {
+        if (data) {
+          // Uncomment this to print out the full response to the console.
+          // logResponse(data);
+          console.log(`\nTotal Participants: ${data.totalParticipants}`);
+        } else {
+          console.log("Error when accessing the API.");
+        }
+      });
+    }
+    else {
+      console.log("Error obtaining access token.");
+    }
+});
